1fe18421fb03ea28e023c13ddc3eb9cf280bc12d
Max Adamski <maxadamski98@gmail.com>
2021-04-01 14:43:19 +0200

    Finish methods for querying relations, add string formatting, expand documentation

--- README.md
+++ README.md
@@ -7,16 +7,59 @@ I created this library, because since version 2.9, Słowosieć cannot be easily 
 
 ## Usage
 
+Load wordnet from an `XML` file (this will take about 20 seconds), and print basic statistics.
+
 ```python
 import plwordnet
 wn = plwordnet.load('plwordnet_4_2.xml')
-# access data:
-# wn.lexical_relations
-# wn.lexical_units
-# wn.synset_relations
-# wn.synsets
-# wn.relation_types
-# wn.relation_types
+print(wn)
+```
+
+Expected output:
+
+```
+PlWordnet
+  lexical units: 513410
+  synsets: 353586
+  relation types: 306
+  synset relations: 1477849
+  lexical relations: 393137
+```
+
+Find lexical units with name `leśny` and print all relations, where where that unit is in the subject/parent position.
+
+```python
+for lu in wn.lemmas('leśny'):
+    for s, p, o in wn.lexical_relations_where(subject=lu):
+        print(p.format(s, o))
+```
+
+Expected output:
+
+```
+leśny.2 tworzy kolokację z polana.1
+leśny.2 jest synonimem mpar. do las.1
+leśny.3 przypomina las.1
+leśny.4 jest derywatem od las.1
+leśny.5 jest derywatem od las.1
+leśny.6 przypomina las.1
+```
+
+Print all relation types and their ids:
+
+```python
+for id, rel in wn.relation_types.items():
+    print(id, rel.name)
+```
+
+Expected output:
+
+```
+10 hiponimia
+11 hiperonimia
+12 antonimia
+13 konwersja
+...
 ```
 
 
@@ -29,7 +72,7 @@ pip install plwordnet
 ```
 
 
-## Supported versions
+## Version support
 
 This library should be able to read future versions of Słowosieć without modification, even if more relation types are added. Still, if you use this library with a version of Słowosieć that is not listed below, please consider contributing information if it is supported.
 
@@ -37,5 +80,32 @@ This library should be able to read future versions of Słowosieć without modif
 	- Simple XML syntax errors
 	- Typo in one attribute key
 	- Typo in one `id` attribute
+- Słowosieć 3.2 - YES
 - Słowosieć 3.0 - YES
+
+
+## Documentation
+
+See `plwordnet/wordnet.py` for `RelationType`, `Synset` and `LexicalUnit` class definitions.
+
+### `Wordnet` instance properties
+
+- `lexical_relations`: list of (subject, predicate, object) triples
+- `synset_relations`: list of (subject, predicate, object) triples
+- `relation_types`: mapping from relation type id to object
+- `lexical_units`: mapping from lexical unit id to unit object
+- `synsets`: mapping from synset id to object
+- `(lexical|synset)_relations_(s|o|p)`: mapping from id of subject/object/predicate to a set of matching lexical unit/synset relation ids
+- `lexical_units_by_name`: mapping from lexical unit name to a set of matching lexical unit ids
+
+### `Wordnet` methods
+
+- `lemmas(value)`: returns a list of `LexicalUnit`, where the name is equal to `value`
+- `load(source)`: reads and indexes Wordnet, where `source` is a path to the wordnet XML file, or a file object opened in binary mode (useful for loading compressed XML files)
+- `lexical_relations_where(subject, predicate, object)`: returns lexical relation triples, with matching subject or/and predicate or/and object. Subject, predicate and object arguments can be integer ids or `LexicalUnit` and `RelationType` objects.
+- `synset_relations_where(subject, predicate, object)`: returns synset relation triples, with matching subject or/and predicate or/and object. Subject, predicate and object arguments can be integer ids or `Synset` and `RelationType` objects.
+
+### `RelationType` methods
+
+- `format(x, y, short=False)`: substitutes `x` and `y` into the `RelationType` display format `display`. If `short`, `x` and `y` are separated by the short relation name `shortcut`.
 
--- plwordnet/wordnet.py
+++ plwordnet/wordnet.py
@@ -1,6 +1,8 @@
+from sys import getsizeof as sizeof
 from lxml import etree
 from dataclasses import dataclass
-from typing import List, Optional
+from collections import defaultdict
+from typing import List, Set, Optional
 
 
 @dataclass
@@ -16,21 +18,34 @@ class RelationType:
     reverse: Optional[int]
     autoreverse: bool
 
+    def format(self, x, y, short=False):
+        if short or not self.display:
+            return f'{x} {self.shortcut} {y}'
+        else:
+            return self.display.replace('<x#>', str(x)).replace('<y#>', str(y))
+
 
 @dataclass
 class Synset:
     id: int
-    lexical_units: List[int]
+    lexical_unit_ids: Set[int]
+    lexical_units: list
     split: int
     definition: str
     description: str
     abstract: bool
     #workstate: str
 
+    def __str__(self):
+        lemmas = ' '.join(str(x) for x in self.lexical_units)
+        return '{#'+str(self.id)+' : '+lemmas+'}'
+
 
 @dataclass
 class LexicalUnit:
     id: int
+    synset_id: Optional[int]
+    synset: list
     name: str
     pos: str
     domain: str
@@ -38,6 +53,9 @@ class LexicalUnit:
     variant: int
     tag_count: int
     #workstate: str
+
+    def __str__(self):
+        return f'{self.name}.{self.variant}'
 
     
 class Wordnet:
@@ -47,74 +65,130 @@ class Wordnet:
         self.relation_types = {}
         self.synset_relations = []
         self.lexical_relations = []
-        
-        self.lexical_relations_s = {}
-        self.lexical_relations_o = {}
-        self.synset_relations_s = {}
-        self.synset_relations_o = {}
+
+        self.lexical_relations_s = defaultdict(set)
+        self.lexical_relations_o = defaultdict(set)
+        self.lexical_relations_p = defaultdict(set)
+        self.synset_relations_s = defaultdict(set)
+        self.synset_relations_o = defaultdict(set)
+        self.synset_relations_p = defaultdict(set)
+        self.lexical_units_by_name = defaultdict(set)
 
     def load(self, source):
         tree = etree.parse(source)
         root = tree.getroot()
-        
+
         for e in root.iter('synsetrelations'):
             a = e.attrib
             self.synset_relations.append((int(a['parent']), int(a['relation']), int(a['child'])))
-            
+
         for e in root.iter('lexicalrelations'):
             a = e.attrib
             self.lexical_relations.append((int(a['parent']), int(a['relation']), int(a['child'])))
-            
+
         for e in root.iter('relationtypes'):
             a = dict(e.attrib)
             id = int(a['id'])
             parent = int(a['parent']) if 'parent' in a else None
             reverse = int(a['reverse']) if 'reverse' in a else None
             self.relation_types[id] = RelationType(
-                id=id, parent=parent, type=a['type'], name=a['name'],
+                id=id, parent=parent, type=a['type'], name=a['name'], pos=a['posstr'].split(','),
                 description=a['description'], shortcut=a['shortcut'], display=a['display'],
-                pos=a['posstr'].split(','), reverse=reverse, autoreverse=bool(a['autoreverse']),
-            )
-            
+                autoreverse=bool(a['autoreverse']), reverse=reverse)
+
         for e in root.iter('lexical-unit'):
             a = dict(e.attrib)
             id = int(a['id'])
             self.lexical_units[id] = LexicalUnit(
-                id=id, name=a['name'], pos=a['pos'], domain=a['domain'], description=a['desc'],
-                variant=int(a['variant']), tag_count=int(a['tagcount']),
-            )
-            
+                id=id, synset_id=None, synset=None, variant=int(a['variant']), tag_count=int(a['tagcount']),
+                name=a['name'], pos=a['pos'], domain=a['domain'], description=a['desc'])
+
         for e in root.iter('synset'):
             a = dict(e.attrib)
             id = int(a['id'])
             self.synsets[id] = Synset(
                 id=id, split=int(a['split']), abstract=bool(a['abstract']),
                 definition=a.get('definition', ''), description=a.get('desc', ''),
-                lexical_units=[x.text for x in e.findall('unit-id')],
-            )
-            
-        del tree
+                lexical_unit_ids=[int(x.text) for x in e.findall('unit-id')],
+                lexical_units=[])
 
-    def index(self):
         for i, x in enumerate(self.synset_relations):
-            s, _, o = x
-            if s not in self.synset_relations_s: self.synset_relations_s[s] = []
-            if o not in self.synset_relations_o: self.synset_relations_o[o] = []
-            self.synset_relations_s[s].append(i)
-            self.synset_relations_o[o].append(i)
-            
+            s, p, o = x
+            self.synset_relations_s[s].add(i)
+            self.synset_relations_o[o].add(i)
+            self.synset_relations_p[p].add(i)
+
         for i, x in enumerate(self.lexical_relations):
-            s, _, o = x
-            if s not in self.lexical_relations_s: self.lexical_relations_s[s] = []
-            if o not in self.lexical_relations_o: self.lexical_relations_o[o] = []
-            self.lexical_relations_s[s].append(i)
-            self.lexical_relations_o[o].append(i)
+            s, p, o = x
+            self.lexical_relations_s[s].add(i)
+            self.lexical_relations_o[o].add(i)
+            self.lexical_relations_p[p].add(i)
+
+        for synset in self.synsets.values():
+            for unit_id in synset.lexical_unit_ids:
+                self.lexical_units[unit_id].synset_id = synset.id
+                self.lexical_units[unit_id].synset = synset
+
+        for unit in self.lexical_units.values():
+            self.lexical_units_by_name[unit.name].add(unit.id)
+            if unit.synset_id is not None:
+                self.synsets[unit.synset_id].lexical_units.append(unit)
+
+    def lexical_relations_where(self, *, subject=None, predicate=None, object=None):
+        if subject is None and predicate is None and object is None:
+            raise Exception('must specify at least subject, predicate or object')
+        found = set(range(len(self.lexical_relations)))
+        if predicate is not None:
+            id = predicate.id if isinstance(predicate, RelationType) else predicate
+            found = found.intersection(self.lexical_relations_p[id])
+        if subject is not None:
+            id = subject.id if isinstance(subject, LexicalUnit) else subject
+            found = found.intersection(self.lexical_relations_s[id])
+        if object is not None:
+            id = object.id if isinstance(object, LexicalUnit) else object
+            found = found.intersection(self.lexical_relations_o[id])
+        results = []
+        for id in found:
+            s, p, o = self.lexical_relations[id]
+            results.append((self.lexical_units[s], self.relation_types[p], self.lexical_units[o]))
+        return results
+
+    def synset_relations_where(self, *, subject=None, predicate=None, object=None):
+        if subject is None and predicate is None and object is None:
+            raise Exception('must specify at least subject, predicate or object')
+        found = set(range(len(self.synset_relations)))
+        if predicate is not None:
+            id = predicate.id if isinstance(predicate, RelationType) else predicate
+            found = found.intersection(self.synset_relations_p[id])
+        if subject is not None:
+            id = subject.id if isinstance(subject, Synset) else subject
+            found = found.intersection(self.synset_relations_s[id])
+        if object is not None:
+            id = object.id if isinstance(object, Synset) else object
+            found = found.intersection(self.synset_relations_o[id])
+        results = []
+        for id in found:
+            s, p, o = self.synset_relations[id]
+            results.append((self.synsets[s], self.relation_types[p], self.synsets[o]))
+        return results
+
+    def lemmas(self, x: str):
+        return [self.lexical_units[id] for id in self.lexical_units_by_name[x]]
+
+    def __repr__(self):
+        props = 'lexical_units synsets relation_types synset_relations lexical_relations'.split()
+        res = 'PlWordnet'
+        for name in props:
+            disp = name.replace('_', ' ')
+            prop = getattr(self, name)
+            res += f'\n  {disp}: {len(prop)}'
+        return res
+
+    __str__ = __repr__
 
 
-def load(source, index=True):
+def load(source):
     wn = Wordnet() 
     wn.load(source)
-    if index:
-        wn.index()
     return wn
 
